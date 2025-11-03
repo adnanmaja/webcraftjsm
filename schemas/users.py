12e71b26d123cfa23dc69a20695cc2abb0e22cc8
Rafa Adnan <rafaadnan46@gmail.com>
2025-11-03 22:31:06 +0700

    Database ready, API baru tabel Users

---
+++ schemas/users.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    role: str = "customer"
+
+class UserUpdate(BaseModel):
+    name: str = None
+    email: str = None
+    password_hash: str = None
+    role: str = None
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    role: str
