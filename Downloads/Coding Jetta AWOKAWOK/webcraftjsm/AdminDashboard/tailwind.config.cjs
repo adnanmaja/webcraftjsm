2e34a9268aab2b6dc0e25efcd4ebb5b9db7ba4c6
jettapindika <jettapindika@gmail.com>
2025-11-08 12:07:41 +0700

    Manajemen Menu

---
+++ Downloads/Coding Jetta AWOKAWOK/webcraftjsm/AdminDashboard/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "system-ui", "sans-serif"],
+        javassoul: ["Javassoul", "cursive", "Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
